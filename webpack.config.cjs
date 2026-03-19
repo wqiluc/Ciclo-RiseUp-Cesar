2278f7d5dbf38cd6f093314465564528a719844f
Lucas Paguetti Pereira <lucaspaguetti2@gmail.com>
2026-03-19 16:32:44 -0300

    continuação projeto hairday

--- webpack.config.cjs
+++ webpack.config.cjs
@@ -1,13 +1,32 @@
 const path = require("path")
+const HtmlWebpackPlugin = require("html-webpack-plugin")
 
 module.exports = {
   entry: "./Nivel_2_JavaScript/2.3-Avançado-pre-FrameWork/projeto Hair Day/src/js/main.js",
+
   output: {
     filename: "bundle.js",
-    path: path.resolve(__dirname, "dist")
+    path: path.resolve(__dirname, "dist"),
+    clean: true
   },
+
   mode: "development",
-  devServer: {
-    static: "./public"
-  }
+
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"]
+      }
+    ]
+  },
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      
+      template: "./Nivel_2_JavaScript/2.3-Avançado-pre-FrameWork/projeto Hair Day/html/index.html",
+
+      favicon: "./Nivel_2_JavaScript/2.3-Avançado-pre-FrameWork/projeto Hair Day/src/assets/scissors.svg"
+    })
+  ]
 }
